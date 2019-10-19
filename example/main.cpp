0c69679bf96ca74775f4866a0544ba8950123556
Marco Nilsson <denravonska@gmail.com>
2019-10-19 10:53:36 +0200

    Update example.

--- example/main.cpp
+++ example/main.cpp
@@ -6,12 +6,13 @@ int main() {
     // LED connected to port B, pin 4.
     IOPin<PortB, 4> led_pin;
     led_pin.setDirection(OUTPUT);
-    led_pin.set();
 
-    // Button connected to port B, pin 5, internal pull-up
+    // Button connected to port B, pin 5, internal pull-up.
+    // Holding the button on start will turn the LED off.
     IOPin<PortB, 4> button_pin;
     button_pin.setDirection(INPUT_PULL_UP);
-    int button_val = button_pin.read();
+    if(button_pin.read())
+        led_pin.set();
 
     while(1);
 }
